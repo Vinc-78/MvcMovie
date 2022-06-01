1ef59e938465432067e570208c7c8be8a2c32485
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2022-06-01 13:03:20 +0200

    Add section with insert file in form

--- Controllers/ImpiegatoController.cs
+++ Controllers/ImpiegatoController.cs
@@ -22,7 +22,44 @@ namespace MvcMovie.Controllers
             return View(TempImpiegato);
         }
 
+        public IActionResult CreaFormConFoto()
+        {
+            Impiegato TempImpiegato = new Impiegato()
+            {
+                Id = 1,
+                FullName = "",
+                Gender = "",
+                City = " ",
+                EmailAddress = "",
+                PersonalWebSite = "",
+                Photo = "",
+                AlternateText = ""
+
+            };
+            return View(TempImpiegato);
+        }
+
         public IActionResult CreaScheda(Impiegato DatiImpiegato)
+        {
+            string sNomeImpiegato = DatiImpiegato.FullName.Split(" ")[0];
+            string sCognomeImpiegato = DatiImpiegato.FullName.Split(" ")[1];
+
+            Impiegato VeroImpiegato = new Impiegato()
+            {
+                Id = DatiImpiegato.Id,
+                FullName = DatiImpiegato.FullName,
+                Gender = DatiImpiegato.Gender,
+                City = DatiImpiegato.City,
+                EmailAddress = sNomeImpiegato + "." + sCognomeImpiegato + "@azienda.it",
+                PersonalWebSite = "www." + sNomeImpiegato + "." + sCognomeImpiegato + ".it",
+                Photo = DatiImpiegato.Photo,
+                AlternateText = "Foto non disponibile "
+
+            };
+            return View(VeroImpiegato);
+        }
+
+        public IActionResult CreaSchedaConFoto(Impiegato DatiImpiegato)
         {
             string sNomeImpiegato = DatiImpiegato.FullName.Split(" ")[0];
             string sCognomeImpiegato = DatiImpiegato.FullName.Split(" ")[1];
