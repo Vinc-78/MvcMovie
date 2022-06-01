07622c26d8a34e5d78dc52f4d648982c7f4dd362
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2022-06-01 12:46:19 +0200

    Aggiunto link e form con view insert & response

---
+++ Controllers/ImpiegatoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class ImpiegatoController : Controller
+    {
+        public IActionResult CreaForm()
+        {
+            Impiegato TempImpiegato = new Impiegato()
+            {
+                Id = 1,
+                FullName = "",
+                Gender = "",
+                City = " ",
+                EmailAddress = "",
+                PersonalWebSite = "",
+                Photo = "",
+                AlternateText = ""
+
+            };
+            return View(TempImpiegato);
+        }
+
+        public IActionResult CreaScheda(Impiegato DatiImpiegato)
+        {
+            string sNomeImpiegato = DatiImpiegato.FullName.Split(" ")[0];
+            string sCognomeImpiegato = DatiImpiegato.FullName.Split(" ")[1];
+
+            Impiegato VeroImpiegato = new Impiegato()
+            {
+                Id = DatiImpiegato.Id,
+                FullName = DatiImpiegato.FullName,
+                Gender = DatiImpiegato.Gender,
+                City = DatiImpiegato.City,
+                EmailAddress = sNomeImpiegato + "." + sCognomeImpiegato + "@azienda.it",
+                PersonalWebSite = "www." + sNomeImpiegato + "." + sCognomeImpiegato + ".it",
+                Photo = DatiImpiegato.Photo,
+                AlternateText = "Foto non disponibile "
+
+            };
+            return View(VeroImpiegato);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Movie}/{action=ShowMovies}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
